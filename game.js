fbd99f4b02d088ef5063d75fe152dfc6d4c474ec
Andrian Kanta <a.kanta22@gmail.com>
2016-06-07 14:15:19 +0800

    2nd pass, changed the layout, updated the logic, and currently suitable for iphone 6plus

--- game.js
+++ game.js
@@ -1,3 +1,44 @@
+// the jQuery ready function will add click listeners once the dom is loaded
+$(function () {
+  console.log('Dom is loaded')
+  $('#ready').click(function () {
+    $('button').toggle()
+    console.log('restart toggle')
+    $('#ready').toggle()
+    $('#question').css('font-size', '40px')
+    // update the display for the first time
+    updateDisplay()
+  })
+  $('#restart').click(function () {
+    restart()
+  })
+  $('button').click(function () {
+      // if gameover then restart else log a player turn
+    $('#question').css('font-size', '40px')
+    if (isGameOver()) {
+      console.log('isgm')
+      restart()
+    } else {
+        // can use jquery index() to find the position of this element in relation to its siblings. works as only answers are in this container
+      playTurn($(this).index())
+      console.log('iam a llama')
+    }
+    updateDisplay()
+    })
+})
+    // var counter = 10
+    //     // var interval = setInterval(function () {
+    //   counter--
+    //   if (counter >= 0) {
+    //     $('#timer_div').innerHTML = counter
+    //   }
+    //     // Display 'counter' wherever you want to display it.
+    //   if (counter === 0) {
+    //     alert('this is where it happens')
+    //     clearInterval(counter)
+    //   }
+    //   }, 1000)
+
 function Question (prompt, answers, correctAnswerIndex) {
   this.prompt = prompt
   this.choices = answers
@@ -14,22 +55,22 @@ var quiz = {
 }
 var randomTotal = 1
 
-var question1 = new Question(generateQn(), [generateRandom(), generateRandom(), randomTotal, generateRandom()], 3)
-var question2 = new Question(generateQn(), [generateRandom(), randomTotal, generateRandom(), generateRandom()], 2)
-var question3 = new Question(generateQn(), [randomTotal, generateRandom(), generateRandom(), generateRandom()], 1)
-var question4 = new Question(generateQn(), [randomTotal, generateRandom(), generateRandom(), generateRandom()], 1)
-var question5 = new Question(generateQn(), [generateRandom(), generateRandom(), randomTotal, generateRandom()], 3)
-var question6 = new Question(generateQn(), [generateRandom(), generateRandom(), randomTotal, generateRandom()], 3)
+var question1 = new Question(generateQn(), [generateRandom(), generateRandom(), randomTotal, generateRandom()], 2)
+var question2 = new Question(generateQn(), [generateRandom(), randomTotal, generateRandom(), generateRandom()], 1)
+var question3 = new Question(generateQn(), [randomTotal, generateRandom(), generateRandom(), generateRandom()], 0)
+var question4 = new Question(generateQn(), [randomTotal, generateRandom(), generateRandom(), generateRandom()], 0)
+var question5 = new Question(generateQn(), [generateRandom(), generateRandom(), randomTotal, generateRandom()], 2)
+var question6 = new Question(generateQn(), [generateRandom(), generateRandom(), randomTotal, generateRandom()], 2)
 
 function generateQn () {
   var randomMath1 = Math.floor(Math.random() * 100)
   var randomMath2 = Math.floor(Math.random() * 100)
-  randomTotal = randomMath1 + randomMath2
-  return randomMath1 + ' + ' + randomMath2
+  randomTotal = randomMath1 * randomMath2
+  return randomMath1 + ' X ' + randomMath2
 }
 
 function generateRandom () {
-  var randomNum = Math.floor(Math.random() * 200)
+  var randomNum = Math.floor(Math.random() * 2000)
   return randomNum
 }
 
@@ -61,19 +102,25 @@ function numberOfAnswers () {
 // It should take a single integer, which specifies which choice the current player wants to make.
 // It should return a boolean true/false if the answer is correct.
 function playTurn (choice) {
+  console.log('playturn is running')
   if (quiz.isGameOver) {
+    console.log('quiz is gameover')
     return false
   } else {
     var playerAnswer = false
-    if (choice === quiz.questions[quiz.currentQuestion].correctChoice) {
+    console.log('playerAnswer')
+    if (choice === correctAnswer()) {
+      console.log('haha')
       playerAnswer = true
-      if (quiz.currentQuestion % 2) {
+      if (currentQuestion() % 2) {
         quiz.player2Points++
+        console.log("Player 2 gets a point")
       } else {
         quiz.player1Points++
+        console.log('Player 1 gets a point')
       }
     } ++quiz.currentQuestion
-    if (quiz.currentQuestion === quiz.questions.length) {
+    if (currentQuestion() === numberOfQuestions()) {
       quiz.isGameOver = true
     }
     return playerAnswer
@@ -110,12 +157,29 @@ function restart () {
     player1Points: 0,
     player2Points: 0
   }
+  $('#question').text('The game restarts!')
+  $('#ready').toggle()
 }
 
 function updateDisplay () {
-
+  if (isGameOver()) {
+    if (whoWon() === 3) {
+      $('#question').text('The game is a draw. Neither player is a winner!')
+      $('#question').css('padding','20px 0')
+    } else {
+      $('#question').text('The winner is player ' + whoWon())
+    }
+  } else {
+    //  Display the question
+    $('#question').text(quiz.questions[quiz.currentQuestion].prompt)
+    //  Display the possible answers
+    console.log('display')
+    for (var i = 0; i <= quiz.questions[quiz.currentQuestion].choices.length; i++) {
+      $('button').eq(i).text(quiz.questions[quiz.currentQuestion].choices[i])
+      console.log('choices')
+    }
+    // Update Player Button
+    $('h3').eq(0).text('Player 1 score : ' + quiz.player1Points)
+    $('h3').eq(1).text('Player 2 score : ' + quiz.player2Points)
+  }
 }
-// the jQuery ready function will add click listeners once the dom is loaded
-$(function () {
-
-})
